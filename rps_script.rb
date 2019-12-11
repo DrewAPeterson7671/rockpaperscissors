6b916d205b042d058d185463bba1a3b3750091ac
Student <example@example.com>
2019-12-11 13:25:02 -0800

    add successful version of playing against computer

--- rps_script.rb
+++ rps_script.rb
@@ -12,12 +12,12 @@ puts "Player 1: #{hand1}"
 
 game = RPS.new()
 
-puts game.computer_move
 hand2 = game.computer_move
+puts "Player 2: #{hand2}"
 
 puts "**********************************"
 
-if hand1 == hand2
+if hand1 === hand2
   puts "This is a draw. :-("
 elsif game.wins?(hand1, hand2) == true
   puts "Player 1 wins!"
