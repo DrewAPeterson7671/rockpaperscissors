f44eea11e558133ee5a65cd9de6c5977cd4a8584
Student <example@example.com>
2019-12-11 14:57:07 -0800

    Finish the project with DRYer code

--- lib/rps.rb
+++ lib/rps.rb
@@ -2,9 +2,7 @@
 class RPS
 
   def computer_move
-    rps_array = ["rock", "paper", "scissors"]
-    rps_array.sample
-
+    rps_array = ["rock", "paper", "scissors"].sample
   end
 
   def hotseat
@@ -12,11 +10,9 @@ class RPS
     hand1 = STDIN.noecho(&:gets).chomp.downcase()
     puts 'Your turn, player 2.'
     hand2 = STDIN.noecho(&:gets).chomp.downcase()
-    @hand1 = hand1
-    @hand2 = hand2
     puts "Player 1: #{hand1}"
     puts "Player 2: #{hand2}"
-    self.resolution
+    self.resolution(hand1, hand2)
 
   end
 
@@ -24,37 +20,33 @@ class RPS
     puts 'Make your move, player 1.'
     hand1 = STDIN.noecho(&:gets).chomp.downcase()
     hand2 = self.computer_move
-    @hand1 = hand1
-    @hand2 = hand2
     puts "Player 1: #{hand1}"
     puts "Player 2: #{hand2}"
-    self.resolution
+    self.resolution(hand1, hand2)
   end
 
   def wins?(hand1, hand2)
-    @hand1 = hand1
-    @hand2 = hand2
-    if (@hand1 == 'rock') & (@hand2 == 'scissors')
+    if (hand1 == 'rock') & (hand2 == 'scissors')
       true
-    elsif (@hand1 == 'rock') & (@hand2 == 'paper')
+    elsif (hand1 == 'rock') & (hand2 == 'paper')
       false
-    elsif (@hand1 == 'paper') & (@hand2 == 'rock')
+    elsif (hand1 == 'paper') & (hand2 == 'rock')
       true
-    elsif (@hand1 == 'paper') & (@hand2 == 'scissors')
+    elsif (hand1 == 'paper') & (hand2 == 'scissors')
       false
-    elsif (@hand1 == 'scissors') & (@hand2 == 'rock')
+    elsif (hand1 == 'scissors') & (hand2 == 'rock')
       false
-    elsif (@hand1 == 'scissors') & (@hand2 == 'paper')
+    elsif (hand1 == 'scissors') & (hand2 == 'paper')
       true
-    elsif @hand1 == @hand2
+    elsif hand1 == hand2
       false
     end
   end
 
-  def resolution
-    if @hand1 == @hand2
+  def resolution(hand1, hand2)
+    if hand1 == hand2
       puts "This is a draw. :-("
-    elsif self.wins?(@hand1, @hand2) == true
+    elsif self.wins?(hand1, hand2) == true
       puts "Player 1 wins!"
     else puts "Player 2 wins!"
     end
--- rps_script.rb
+++ rps_script.rb
@@ -5,17 +5,18 @@ require 'io/console'
 
 game = RPS.new()
 
+puts `clear`
 puts "=================================="
 puts "=================================="
 puts "  Rock! Paper!  and SCISSORS!!!!"
 puts "=================================="
 puts "=================================="
-puts "Enter 1) to play the computer!"
-puts "Enter 2) to play a friend!"
+puts "Enter 1 to play the computer!"
+puts "Enter 2 to play a friend!"
 
 num_players = gets.chomp
 puts "**********************************"
-puts "lets get ready to RUMMMMBBBBLLLLEE"
+puts "let's get ready to RUMMMMBBBBLLLLEE"
 puts "**********************************"
 
 if num_players == "1"
@@ -25,22 +26,6 @@ elsif num_players == "2"
 else
   puts "wha..???"
 end
-
-# hand2 = gets.chomp
-
-
-# puts "Player 1: #{@hand1}"
-# puts "Player 2: #{@hand2}"
-
-# hand2 = game.computer_move
-
-
-# if hand1 == hand2
-#   puts "This is a draw. :-("
-# elsif game.wins?(hand1, hand2) == true
-#   puts "Player 1 wins!"
-# else puts "Player 2 wins!"
-# end
 
 puts "=================================="
 puts "=================================="
